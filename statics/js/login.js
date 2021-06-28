398e57d3186c2e45d931e174fb093620968c9989
sleetwalker <79039010+sleetwalker@users.noreply.github.com>
2021-06-28 17:20:48 +0800

    Add files via upload

---
+++ statics/js/login.js
@@ -0,0 +1,11 @@
+$(function() {
+	// Waves初始化
+	Waves.displayEffect();
+	// 输入框获取焦点后出现下划线
+	$('.form-control').focus(function() {
+		$(this).parent().addClass('fg-toggled');
+	}).blur(function() {
+		$(this).parent().removeClass('fg-toggled');
+	});
+});
+c.init();
